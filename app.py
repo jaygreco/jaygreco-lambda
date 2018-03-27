f6478c19c7e74a166822028f20bf83b09ef3b23d
Jay Greco <jayv.greco@gmail.com>
2018-03-26 23:34:09 -0600

    Added Flask-S3 to serve assets from the project S3 bucket.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, send_from_directory
-app = Flask(__name__,  static_url_path='')
+from flask import Flask, render_template, send_from_directory, url_for
+from flask_s3 import FlaskS3
 
+app = Flask(__name__)
+
+#Set the S3 bucket name for the site's static resources
+app.config['FLASKS3_BUCKET_NAME'] = 'com-jaygreco-zappa'
+
+#NOTE: This is needed in order to force Flask-S3 to set the MIME type during upload.
+#Without this line, CSS on S3 will not be served because of modern browser security.
+app.config['FLASKS3_FORCE_MIMETYPE'] = True
+
+#Instantiate flask-s3 to handle static resources
+s3 = FlaskS3(app)
+
+#The one and only route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    print path
-    return send_from_directory('static', path)
-
-# We only need this for local development.
+#This is only needed for local development.
 if __name__ == '__main__':
     app.run(debug=True)
